1e868acb2f1daa0ee37ed164091f757660d2c6fe
Dong Zhou <dong.zhou.08@gmail.com>
2015-03-16 19:22:00 -0400

    add pktgen

--- tools/pktgen/pktgen.c
+++ tools/pktgen/pktgen.c
@@ -38,6 +38,9 @@
 #include <rte_ether.h>
 #include <rte_ethdev.h>
 #include <rte_string_fns.h>
+#include <rte_ip.h>
+#include <rte_udp.h>
+#include <rte_byteorder.h>
 
 #define MAX_LCORE (RTE_MAX_LCORE)
 
@@ -499,83 +502,156 @@ parse_args(int argc, char **argv)
     }
 }
 
+#define UDP_SRC_PORT (1024)
+#define UDP_DST_PORT (1024)
+
+#define IP_SRC_ADDR ((192U << 24) | (168 << 16) | (0 << 8) | 1)
+#define IP_DST_ADDR ((192U << 24) | (168 << 16) | (0 << 8) | 2)
+
+#define IP_DEFTTL  64   /* from RFC 1340. */
+#define IP_VERSION 0x40
+#define IP_HDRLEN  0x05 /* default IP header length == five 32-bits words. */
+#define IP_VHL_DEF (IP_VERSION | IP_HDRLEN)
+
+#define ENABLE_FLOW_CONTROL
+
+unsigned flow_limit = 5000;
+
+static void
+make_udp_pkt(portid_t port_id, struct rte_mbuf *pkt)
+{
+    uint16_t pkt_len = 60;
+        
+    struct ether_hdr *eth_hdr = rte_pktmbuf_mtod(pkt, struct ether_hdr *);
+    struct ipv4_hdr *ip_hdr = (struct ipv4_hdr *)((char *)eth_hdr + sizeof(struct ether_hdr));
+    struct udp_hdr *udp_hdr = (struct udp_hdr *)((char *)ip_hdr + sizeof(struct ipv4_hdr));
+    char *payload = (char *)((char *)udp_hdr + sizeof(struct udp_hdr));
+
+    memset(payload, 0, pkt_len - sizeof(struct ether_hdr) - sizeof(struct ipv4_hdr) - sizeof(struct udp_hdr));
+
+    eth_hdr->ether_type = rte_cpu_to_be_16(ETHER_TYPE_IPv4);
+    memcpy(&eth_hdr->s_addr, &port_data[port_id].ethaddr, sizeof(struct ether_addr));
+    eth_hdr->d_addr.addr_bytes[0] = 0x00;
+    eth_hdr->d_addr.addr_bytes[1] = 0x0C;
+    eth_hdr->d_addr.addr_bytes[2] = 0x29;
+    eth_hdr->d_addr.addr_bytes[3] = 0x40;
+    eth_hdr->d_addr.addr_bytes[4] = 0x20;
+    eth_hdr->d_addr.addr_bytes[5] = 0x59;
+
+    ip_hdr->src_addr = IP_SRC_ADDR;
+    ip_hdr->dst_addr = IP_DST_ADDR;
+    ip_hdr->version_ihl = IP_VHL_DEF;
+    ip_hdr->type_of_service = 0;
+    ip_hdr->fragment_offset = 0;
+    ip_hdr->time_to_live = IP_DEFTTL;
+    ip_hdr->next_proto_id = IPPROTO_UDP;
+    ip_hdr->packet_id = 0;
+    ip_hdr->total_length = rte_cpu_to_be_16(pkt_len - sizeof(struct ether_hdr));
+
+    udp_hdr->src_port = UDP_SRC_PORT;
+    udp_hdr->dst_port = UDP_DST_PORT;
+    udp_hdr->dgram_len = rte_cpu_to_be_16(pkt_len - sizeof(struct ether_hdr) - sizeof(struct ipv4_hdr));
+    udp_hdr->dgram_cksum = 0;
+        
+    uint16_t *ptr16 = (uint16_t *)ip_hdr;
+    uint32_t ip_cksum;
+    ip_cksum = 0;
+    ip_cksum += ptr16[0]; ip_cksum += ptr16[1];
+    ip_cksum += ptr16[2]; ip_cksum += ptr16[3];
+    ip_cksum += ptr16[4];
+    ip_cksum += ptr16[6]; ip_cksum += ptr16[7];
+    ip_cksum += ptr16[8]; ip_cksum += ptr16[9];
+
+    /*
+     * reduce 32 bit checksum to 16 bits and complement it
+     */
+    ip_cksum = ((ip_cksum & 0xFFFF0000) >> 16) + (ip_cksum & 0x0000FFFF);
+    if (ip_cksum > 65535)
+        ip_cksum -= 65535;
+    ip_cksum = (~ip_cksum) & 0x00005555;
+    if (ip_cksum == 0)
+        ip_cksum = 0xFFFF;
+    ip_hdr->hdr_checksum = (uint16_t)ip_cksum;
+
+    pkt->pkt.data_len = pkt_len;
+    pkt->pkt.next = NULL; /* last segment of packet */
+    pkt->pkt.nb_segs = 1;
+    pkt->pkt.pkt_len = pkt_len;
+    pkt->ol_flags = 0;
+}
+
 lcoreid_t master_lcore_id;
 
-uint64_t port_ipackets[MAX_PORT], prev_port_ipackets[MAX_PORT];
-uint64_t port_opackets[MAX_PORT], prev_port_opackets[MAX_PORT];
-
 static int
-echo_loop(void *arg)
+pktgen_loop(void *arg)
 {
     lcoreid_t lcore_id = rte_lcore_id();
     unsigned socket_id = rte_lcore_to_socket_id(lcore_id);
     portid_t port_id;
-    queueid_t queue_id;
-    struct rte_mbuf *pkts[DEFAULT_PKT_BURST];
-    uint16_t num_pkts;
-    uint64_t prev_tsc, cur_tsc, start_tsc;
+    struct rte_mbuf *pkt;
+    uint64_t cur_tsc, start_tsc, next_tsc;
     uint64_t tsc_hz = rte_get_tsc_hz();
+#ifdef ENABLE_FLOW_CONTROL
+    unsigned limit = num_enabled_ports * flow_limit;
+#endif
 
-    prev_tsc = start_tsc = rte_rdtsc();
+    start_tsc = rte_rdtsc();
+    next_tsc = start_tsc + tsc_hz;
 
     while (1) {
         cur_tsc = rte_rdtsc();
 
-        if (cur_tsc - prev_tsc > tsc_hz) {
+        if (cur_tsc >= next_tsc) {
             if (lcore_id == master_lcore_id) {
-                uint64_t ipackets = 0, opackets = 0;
+                uint64_t thruput = 0;
 
                 for (port_id = 0; port_id < num_ports; ++port_id) {
                     if (!((enabled_port_mask >> port_id) & 1))
                         continue;
 
                     struct rte_eth_stats stats;
-                    /* rte_eth_stats_get(port_id, &stats); */
-                    ipackets += port_ipackets[port_id] - prev_port_ipackets[port_id];
-                    opackets += port_opackets[port_id] - prev_port_opackets[port_id];
-                    prev_port_ipackets[port_id] = port_ipackets[port_id];
-                    prev_port_opackets[port_id] = port_opackets[port_id];
+                    rte_eth_stats_get(port_id, &stats);
+                    thruput += stats.opackets - port_data[port_id].stats.opackets;
+                    port_data[port_id].stats = stats;
                 }
 
                 printf("%04llu ", (cur_tsc - start_tsc) / tsc_hz);
-                if (ipackets >= 100000)
-                    printf("ipackets:%.2lfM", (double)ipackets / 1000000.0);
+                if (thruput >= 100000)
+                    printf(" thruput=%.2lfM", ((double)thruput) / 1000000.0);
                 else
-                    printf("ipackets:%llu", ipackets);
-                printf(",");
-                if (opackets >= 100000)
-                    printf("opackets:%.2lfM", (double)opackets / 1000000.0);
-                else
-                    printf("opackets:%llu", opackets);
+                    printf(" thruput=%llu", thruput);
                 printf("\n");
                 fflush(stdout);
             }
 
-            prev_tsc = cur_tsc;
+            next_tsc += tsc_hz;
+
+#ifdef ENABLE_FLOW_CONTROL
+            limit = num_enabled_ports * flow_limit;
+#endif
         }
 
-        unsigned i, queue;
-
-        for (queue = 0; queue < lcore_data[lcore_id]->num_rx_queues; ++queue) {
-            port_id = lcore_data[lcore_id]->rx_queue_list[queue].port_id;
-            queue_id = lcore_data[lcore_id]->rx_queue_list[queue].queue_id;
+        for (port_id = 0; port_id < num_ports; ++port_id) {
             if (!((enabled_port_mask >> port_id) & 1))
                 continue;
-
-            num_pkts = recv_pkts(port_id, queue_id, pkts, DEFAULT_PKT_BURST);
-            port_ipackets[port_id] += num_pkts;
                         
-            for (i = 0; i < num_pkts; ++i)
-                send_pkt(port_id, pkts[i]);
-            port_opackets[port_id] += num_pkts;
-        }
+#ifdef ENABLE_FLOW_CONTROL
+            if (limit > 0)
+                --limit;
+            else
+                break;
+#endif
+            pkt = alloc_pkt(lcore_data[lcore_id]->mbp);
+            make_udp_pkt(port_id, pkt);
+            send_pkt(port_id, pkt);
+        }               
     }
 }
 
 int main(int argc, char **argv)
 {
     portid_t port_id;
-    int ret;
+    int ret, leading = 1;
 
     rte_set_log_level(RTE_LOG_NOTICE);
 
@@ -603,13 +679,27 @@ int main(int argc, char **argv)
     argc -= ret;
     argv += ret;
     parse_args(argc, argv);
-    num_enabled_ports = __builtin_popcountll(enabled_port_mask);
 
     init_config();
     start_port();
 
     master_lcore_id = rte_get_master_lcore();
-    rte_eal_mp_remote_launch(echo_loop, NULL, CALL_MASTER);
+    printf("master_lcore_id=%d\n", master_lcore_id);
+
+    printf("tx_burst_size=%d\n", tx_burst_size);
+    printf("enabled_ports=");
+    num_enabled_ports = 0;
+    for (port_id = 0; port_id < num_ports; ++port_id)
+        if ((enabled_port_mask >> port_id) & 1) {
+            if (!leading)
+                putchar(',');
+            leading = 0;
+            printf("%u", port_id);
+            ++num_enabled_ports;
+        }
+    printf("\n");
+
+    rte_eal_mp_remote_launch(pktgen_loop, NULL, CALL_MASTER);
     rte_eal_mp_wait_lcore();
 
     return 0;
